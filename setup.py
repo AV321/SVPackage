f65b2c9abd9a17c6895362ebb3129aec6c986a0c
AV321 <avitko@stanford.edu>
2017-12-19 16:17:31 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup 
+
+setup(
+        name = 'SVassembly',
+        packages = ['SVassembly'],
+        version = '0.1dev',
+        description = 'SV phasing pipeline',
+        author = 'Hanlee Ji lab'
+        author_email = 'avitko@stanford.edu',
+        #url
+        #dowload_url
+        #keywords
+        #classifiers
+)
