d34e75837cbf506edeb686b32458384a1950fa6c
AV321 <avitko@stanford.edu>
2017-12-27 14:29:27 -0500

    Update __init__.py

--- SVassembly/__init__.py
+++ SVassembly/__init__.py
@@ -3,3 +3,6 @@ from InterestingContigs import interestingContigs
 
 from SVassembly import plotting
 from plotting import map_to_genome
+
+from SVassembly import generate_bam2fq_script
+from generate_bam2fq_script import extract_fastq
