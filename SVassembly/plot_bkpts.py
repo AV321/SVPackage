5fef42debcff1fffcd4c97e6def2e99583cb2032
AV321 <avitko@stanford.edu>
2017-12-30 14:21:19 -0500

    Update plot_bkpts.py

--- SVassembly/plot_bkpts.py
+++ SVassembly/plot_bkpts.py
@@ -2,8 +2,16 @@ import rpy2
 import readline #what is this? seems to fix robjects problem
 from rpy2 import robjects 
 
-def plot(bkpt_name):
+#OR
+import os
+import subprocess
+
+"""def plot(bkpt_name):
     r = robjects.r
     r.source("plot_bcs_across_bkpts.R")
     r.Rplot_bcs(bkpt_name)
-    #r.rfunc(bkpt_name)
+    #r.rfunc(bkpt_name)"""
+
+#OR
+args = ['R','plot_bcs_across_bkpts.R', '1']
+process = subprocess.Popen(args, stdout = sys.stdout)
