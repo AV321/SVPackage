2ab6366d96bb1dbfa30dd3d39de82535a1a0d6d2
AV321 <avitko@stanford.edu>
2017-12-30 11:27:04 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
         name = 'SVassembly',
         packages = ['SVassembly'],
-        version = '0.413',
+        version = '0.414',
         description = 'SV phasing pipeline',
         author = 'Hanlee Ji lab',
         author_email = 'avitko@stanford.edu',
         url = 'https://github.com/AV321/SVassembly',
-        dowload_url = 'https://github.com/AV321/SVassembly/archive/0.413.tar.gz',
+        dowload_url = 'https://github.com/AV321/SVassembly/archive/0.414.tar.gz',
         license = 'MIT'
         #install_requires = ['pandas']
         #keywords
