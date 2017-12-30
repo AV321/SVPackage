0f38e5d9a1ba43e23bca5526d4ecec22f36be7d0
AV321 <avitko@stanford.edu>
2017-12-30 14:52:29 -0500

    Update plot_bkpts.py

--- SVassembly/plot_bkpts.py
+++ SVassembly/plot_bkpts.py
@@ -6,12 +6,12 @@ from rpy2 import robjects
 import os
 import subprocess
 
-"""def plot(bkpt_name):
-    r = robjects.r
+def plot(bkpt_name):
+    """r = robjects.r
     r.source("plot_bcs_across_bkpts.R")
     r.Rplot_bcs(bkpt_name)
     #r.rfunc(bkpt_name)"""
 
 #OR
-args = ['R','plot_bcs_across_bkpts.R', '1']
-process = subprocess.Popen(args) #, stdout = sys.stdout)
+    args = ['R','plot_bcs_across_bkpts.R', '1']
+    process = subprocess.Popen(args) #, stdout = sys.stdout)
