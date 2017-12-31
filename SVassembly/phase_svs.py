ec981a3ed3b77c04d7e770c954c47d78e1572789
AV321 <avitko@stanford.edu>
2017-12-30 23:52:19 -0500

    Update phase_svs.py

--- SVassembly/phase_svs.py
+++ SVassembly/phase_svs.py
@@ -256,11 +256,13 @@ def phase(vcf_norm_input, vcf_tum_input, sv_input, bam_input, outpre, window_siz
 
 	global vcf_reader_norm 
 	vcf_reader_norm = vcf.Reader(filename=vcf_norm_input)
+	global norm_smpl 
 	norm_smpl = vcf_reader_norm.samples[0]
 	vcf_data_norm = []
 
 	global vcf_reader_tum
 	vcf_reader_tum = vcf.Reader(filename=vcf_tum_input)
+	global tum_smpl 
 	tum_smpl = vcf_reader_tum.samples[0]
 	vcf_data_tum = []
 
