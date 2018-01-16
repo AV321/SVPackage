e6d7b158378152fed5571ccd3ae84ac93d05e728
AV321 <avitko@stanford.edu>
2018-01-16 11:23:11 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
         dowload_url = 'https://github.com/AV321/SVassembly/archive/0.429.tar.gz',
         license = 'MIT',
         package_data = {'SVassembly': ['*.r', '*.R']},#'*.Rscript']}, #added bc of R file
-        include_package_data=True  #added bc of R file
+        #include_package_data=True  #added bc of R file
         #install_requires = ['pandas']  #need to fill this out
         #keywords
         #classifiers
