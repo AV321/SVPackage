1da8639d4535e4f021b021cce7c7f0d0f96776e7
AV321 <avitko@stanford.edu>
2018-03-09 16:00:06 -0800

    Update __init__.py

--- SVassembly/__init__.py
+++ SVassembly/__init__.py
@@ -14,10 +14,10 @@ from SVassembly import plotting
 from plotting import map_to_genome
 
 from SVassembly import extract_reads_2_0
-from generate_bam2fq_script import extract_readsv2_0  #LR v2.0
+from extract_reads_2_0 import extract_readsv2_0  #LR v2.0
 
-from SVassembly import extract_reads_by_barcode_fastq_noninterleaved
-from extract_reads_by_barcode_fastq_noninterleaved import extract_readsv2_1 #LR v2.1
+from SVassembly import extract_reads_by_barcode_2_1
+from extract_reads_by_barcode_2_1 import extract_readsv2_1 #LR v2.1
 
 from SVassembly import InterestingContigs
 from InterestingContigs import interestingContigs
