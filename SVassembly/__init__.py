01600e4122b0445837e6ea51e2a162fb62afd747
AV321 <avitko@stanford.edu>
2018-04-06 11:12:13 -0400

    Update __init__.py

--- SVassembly/__init__.py
+++ SVassembly/__init__.py
@@ -13,11 +13,17 @@ from count_bcs_f import count_bcs #can't have "-"
 from SVassembly import plotting
 from plotting import map_to_genome
 
-from SVassembly import extract_reads_2_0
-from extract_reads_2_0 import extract_readsv2_0  #LR v2.0
+from SVassembly import extract_reads_2_0_new
+from extract_reads_2_0_new import extract_readsv2_0  #LR v2.0
 
-#from SVassembly import extract_reads_by_barcode_2_1 #uncomment these
-#from extract_reads_by_barcode_2_1 import extract_readsv2_1 #LR v2.1
+from SVassembly import extract_reads_2_0_old
+from extract_reads_2_0_old import extract_readsv2_0  #LR v2.0
+
+#from SVassembly import extract_reads_by_barcode_2_1_new #uncomment these
+#from extract_reads_by_barcode_2_1_new import extract_readsv2_1 #LR v2.1
+
+#from SVassembly import extract_reads_by_barcode_2_1_old #uncomment these
+#from extract_reads_by_barcode_2_1_new import extract_readsv2_1#LR v2.1
 
 from SVassembly import InterestingContigs
 from InterestingContigs import interestingContigs
